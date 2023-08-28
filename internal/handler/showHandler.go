43d2f5b1246c8a8b103a7991c94742388f003efe
YiZou89 <zouyi89@outlook.com>
2023-08-28 12:39:36 +0800

    init

---
+++ internal/handler/showHandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/YiZou89/shortener/internal/logic"
+	"github.com/YiZou89/shortener/internal/svc"
+	"github.com/YiZou89/shortener/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func ShowHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.ShowRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewShowLogic(r.Context(), svcCtx)
+		resp, err := l.Show(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
